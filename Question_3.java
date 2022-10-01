427efa003a8763c427afc9331dac32b1d09df8ad
sarika2011 <114053434+sarika2011@users.noreply.github.com>
2022-10-01 08:17:24 +0530

    Add files via upload

---
+++ Question_3.java
@@ -0,0 +1,13 @@
+package Question3;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Question_3 {
+
+    //Use rangeClosed to create a  Stream
+    public static void main(String[] args) {
+
+        IntStream.rangeClosed(1,10)
+                .forEach(System.out::println);
+    }
+}
