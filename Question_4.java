9305d5cf5d4987d52e9956e7b4fa431a13cde42b
sarika2011 <114053434+sarika2011@users.noreply.github.com>
2022-10-01 08:24:48 +0530

    Add files via upload

---
+++ Question_4.java
@@ -0,0 +1,15 @@
+package Question4;
+
+import java.util.stream.IntStream;
+
+public class Question_4 {
+
+    //Use iterator stream method to generate a stream
+
+    public static void main(String[] args) {
+
+        IntStream
+                .iterate(0,i->i<=10,i->i+1)
+                .forEach(System.out::println);
+    }
+}
