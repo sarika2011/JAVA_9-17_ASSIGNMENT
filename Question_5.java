2399bff2fda679199facd8b0dbeaa309d3ed9eee
sarika2011 <114053434+sarika2011@users.noreply.github.com>
2022-10-01 08:25:31 +0530

    Add files via upload

---
+++ Question_5.java
@@ -0,0 +1,24 @@
+package Question5;
+
+import java.util.stream.Stream;
+
+public class Question_5 {
+
+    //Use ifPresentOrElse, or, orElseThrow Operations with Optional
+
+    public static void main(String[] args) {
+
+
+        Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9)
+                .filter(e -> e > 8)
+                .findFirst()
+                .ifPresentOrElse(System.out::println,
+                        () -> System.out.println("Value doesn't exists"));
+
+
+        Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9)
+                .filter(e -> e > 8)
+                .findFirst()
+                .orElseThrow();
+    }
+}
