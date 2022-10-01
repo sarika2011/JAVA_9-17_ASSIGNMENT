7a0cbf151054ace5ef441b828198f74e7134a475
sarika2011 <114053434+sarika2011@users.noreply.github.com>
2022-10-01 08:33:29 +0530

    Add files via upload

---
+++ Question_12.java
@@ -0,0 +1,22 @@
+package Question12;
+
+sealed class Base permits Child1, Child2 {
+};
+
+final class Child1 extends Base {
+};
+
+sealed class Child2 extends Base permits Child3 {
+};
+
+non-sealed class Child3 extends Child2 {
+};
+
+public class Question_12 {
+
+    //Demonstrate the use of Sealed Classes.
+
+    public static void main(String[] args) {
+
+    }
+}
